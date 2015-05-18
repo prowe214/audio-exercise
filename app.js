2a5f67514f29b95918cf84506c6bf7d9b4a2ba36
Conner Alegre <Conner.Alegre@gmail.com>
2015-05-18 12:00:55 -0600

    Added buttons

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var playButton = document.getElementById("play");
-var audio = document.getElementById("audio");
-playButton.addEventListener('click', function() {
-  audio.play();
+var playButtonA = document.getElementById("playA");
+var audioA = document.getElementById("audioA");
+var playButtonB = document.getElementById("playB");
+var audioB = document.getElementById("audioB");
+playButtonA.addEventListener('click', function() {
+  audioA.play();
+});
+
+playButtonB.addEventListener('click', function() {
+  audioB.play();
 });
